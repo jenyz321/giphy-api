4d45a7986831a80a90920ec0558feae8500538f3
JenyZ <jenniferzirk@gmail.com>
2019-04-25 21:25:36 -0400

    connecting to the API

--- giphy.js
+++ giphy.js
@@ -1,97 +1,101 @@
-$(document).ready(function() {
+var pizza= ["pepperoni", "pineapple", "sausage", "cheese"];
+// var queryURL= "https://api.giphy.com/v1/gifs/search";
 
-    var animals = [
-      "alligator", "monkey", "turtle", "gecko", "cat", "angelfish",
-      "turkey", "ape", "dog", "sugar glider", "cow",
-      "shark", "hamster", "snake", "pig", "peacock",
-      "cougar", "hyena", "lion", "fish", "bear"
-    ];
+
+$(document).ready(function() {
+    // make buttons and add to page
+    function renderButtons() {
+      $("#buttons-view").empty();
   
-    // function to make buttons and add to page
-    function populateButtons(arrayToUse, classToAdd, areaToAddTo) {
-      $(areaToAddTo).empty();
-  
-      for (var i = 0; i < arrayToUse.length; i++) {
+      for (var i = 0; i < pizza.length; i++) {
         var a = $("<button>");
-        a.addClass(classToAdd);
-        a.attr("data-type", arrayToUse[i]);
-        a.text(arrayToUse[i]);
-        $(areaToAddTo).append(a);
+        a.addClass("btn btn-danger buttons");
+        a.attr("data-name", pizza[i]);
+        a.text(pizza[i]);
+        $("#buttons-view").append(a);
       }
-  
-    }
-    $(".animal-button").on('click', function(){
+     
+    };
+    renderButtons();
+    $("#add-pizza").on('click', function(event){
+      event.preventDefault();
+      console.log("pizza button clicked");
       
-    })
-    $(document).on("click", ".animal-button", function() {
-      $("#animals").empty();
-      $(".animal-button").removeClass("active");
-      $(this).addClass("active");
+      var addPizza = $("#pizza-input").val().trim();
+      pizza.push(addPizza);
+      console.log(addPizza);
+      console.log(pizza);
+      renderButtons();
+    });
+    $(document).on("click", ".buttons", function() {
+      $("#images").empty();
+      // $(".buttons").removeClass("active");
+      // $(this).addClass("active");
   
-      var type = $(this).attr("data-type");
-      var year = $(this).attr("data-year");
-      var queryURL = "http://api.giphy.com/v1/gifs/search?q=" + type + "year="+ year +"&api_key=tQOzcKR23rsAFUi1ZeJDswsl3Z6gUReY";
-  
+      var word = $(this).attr("data-name");
       
+      var queryURL = "http://api.giphy.com/v1/gifs/search?q=" + word + "&api_key=tQOzcKR23rsAFUi1ZeJDswsl3Z6gUReY";
+      console.log(word);
+      console.log(queryURL);
+    }) 
       $.ajax({
         url: queryURL,
         method: "GET"
-      })
-        .then(function(response) {
+      }).then(function(response) {
           var results = response.data;
           console.log(results);
-          for (var i = 0; i < results.length; i++) {
-            var animalDiv = $("<div class=\"animal-item\">");
+    //       for (var i = 0; i < results.length; i++) {
+    //         var pizzaDiv = $("<div class=\"pizza-item\">");
   
-            var rating = results[i].rating;
+    //         var rating = results[i].rating;
   
-            var p = $("<p>").text("Rating: " + rating);
+    //         var p = $("<p>").text("Rating: " + rating);
   
-            var animated = results[i].images.fixed_height.url;
-            var still = results[i].images.fixed_height_still.url;
+    //         var animated = results[i].images.fixed_height.url;
+    //         var still = results[i].images.fixed_height_still.url;
   
-            var animalImage = $("<img>");
-            animalImage.attr("src", still);
-            animalImage.attr("data-still", still);
-            animalImage.attr("data-animate", animated);
-            animalImage.attr("data-state", "still");
-            animalImage.addClass("animal-image");
+    //         var pizzaImage = $("<img>");
+    //         pizzaImage.attr("src", still);
+    //         pizzaImage.attr("data-still", still);
+    //         pizzaImage.attr("data-animate", animated);
+    //         pizzaImage.attr("data-state", "still");
+    //         pizzaImage.addClass("pizza-image");
   
   
-            animalDiv.append(p);
-            animalDiv.append(animalImage);
+    //         pizzaDiv.append(p);
+    //         pizzaDiv.append(pizzaImage);
   
-            $("#animals").append(animalDiv);
-          }
-        });
+    //         $("#pizzas").append(pizzaDiv);
+    //       }
+    //     });
+    // });
+  
+    // $(document).on("click", ".pizza-image", function() {
+  
+    //   var state = $(this).attr("data-state");
+  
+    //   if (state === "still") {
+    //     $(this).attr("src", $(this).attr("data-animate"));
+    //     $(this).attr("data-state", "animate");
+    //   }
+    //   else {
+    //     $(this).attr("src", $(this).attr("data-still"));
+    //     $(this).attr("data-state", "still");
+    //   }
+    // });
+  
+    // $("#add-pizza").on("click", function(event) {
+    //   event.preventDefault();
+    //   var newpizza = $("input").eq(0).val();
+  
+    //   if (newpizza.length > 2) {
+    //     pizza.push(newpizza);
+    //   }
+  
+    //   // populateButtons(pizzas, "pizza-button", "#pizza-buttons");
+  
     });
+});
+    
   
-    $(document).on("click", ".animal-image", function() {
-  
-      var state = $(this).attr("data-state");
-  
-      if (state === "still") {
-        $(this).attr("src", $(this).attr("data-animate"));
-        $(this).attr("data-state", "animate");
-      }
-      else {
-        $(this).attr("src", $(this).attr("data-still"));
-        $(this).attr("data-state", "still");
-      }
-    });
-  
-    $("#add-animal").on("click", function(event) {
-      event.preventDefault();
-      var newAnimal = $("input").eq(0).val();
-  
-      if (newAnimal.length > 2) {
-        animals.push(newAnimal);
-      }
-  
-      populateButtons(animals, "animal-button", "#animal-buttons");
-  
-    });
-  
-    populateButtons(animals, "animal-button", "#animal-buttons");
-  });
   
